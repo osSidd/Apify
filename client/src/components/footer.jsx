806d4cad3b93f005f7876e1141ae441adea041f0
Osama <107386980+osSidd@users.noreply.github.com>
2023-10-10 10:26:13 +0530

    changed footer layout column to row

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -15,53 +15,55 @@ export default function Footer(){
         <Container 
             maxWidth="xl"
             sx={{
-                bgcolor: '#222',
+                bgcolor: '#333',
                 py:2,
             }} 
         >
             <Box
                 sx={{
-                    my: 8,
+                    mt: 2,
+                    mb:2,
+                    pb:2,
+                    borderBottom:'1px solid #555'
                 }}
             >
                 <Grid container alignItems="start" px={5} columnGap={15}>
-                    <Grid item xs={12} md={3} display="flex" flexDirection="column" alignItems="start"> 
+                    <Grid item xs={12} md={12} mb={2} display="flex" flexDirection="row" alignItems="center" justifyContent="center"> 
                         <LogoText/>
-                        <Typography
+                        {/* <Typography
                             sx={{color: '#eee'}}
                             mt={2}
                         >
                             A PWA which brings feeds of news, weather, sports, finance, social and enterntainment under a single hood and thus eliminates the need to have multiple apps.
-                        </Typography>
+                        </Typography> */}
                     </Grid>
-                    <Grid item xs={12} md={1} display="flex" flexDirection="column">
-                        <Typography
+                    <Grid item xs={12} md={12} display="flex" flexDirection="row" justifyContent="center">
+                        {/* <Typography
                             sx={{color: '#eee'}}
                             variant='h6'
                             mb={2}
                             alignSelf="start"
                         >
                             Pages
-                        </Typography>
-                        <Box display="flex" flexDirection="column" alignItems="start">
+                        </Typography> */}
+                        <Box display="flex" flexDirection="row" alignItems="center">
                             <NavLinks/>
                         </Box>
                     </Grid>
-                    <Grid item xs={12} md={1} display="flex" flexDirection="column">
-                        <Typography
+                    <Grid item xs={12} md={12} display="flex" flexDirection="row" justifyContent="center">
+                        {/* <Typography
                             sx={{color: '#eee'}}
                             variant='h6'
                             mb={2}
                         >
                             Links
-                        </Typography>
-                        <Box display="flex" flexDirection="column" alignItems="start">
+                        </Typography> */}
+                        <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
                             {
                                 links.map(link => (
                                     <Button
                                         component={RouterLink}
-                                        sx={{color:'white', marginRight:'auto'}}
-                                        
+                                        sx={{color:'#ddd', marginRight:'auto', fontSize:'12px'}}                                        
                                     >
                                         {link}
                                     </Button>
@@ -69,30 +71,30 @@ export default function Footer(){
                             }
                         </Box>
                     </Grid>
-                    <Grid item xs={12} md={3} display="flex" flexDirection="column">
-                        <Typography
+                    <Grid item xs={12} md={12} display="flex" flexDirection="column">
+                        {/* <Typography
                             sx={{color: '#eee'}}
                             variant='h6'
                             mb={2}
                         >
                             Get in Touch
-                        </Typography>
-                        <TextField size='small' sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Name'/>
+                        </Typography> */}
+                        {/* <TextField size='small' sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Name'/>
                         <TextField size='small' sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Email'/>
                         <TextField size='small' multiline rows={4} sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Message'/>
-                        <Button variant='contained' color='primary'>Send Message</Button>
-                        <Box m="24px auto 0">
+                        <Button variant='contained' color='primary'>Send Message</Button> */}
+                        <Box m="auto">
                             <IconButton sx={{color:'#fff'}} href='https://www.linkedin.com/in/osama-siddiquee-5722a1a1/' target='_blank'>
-                                <LinkedInIcon sx={{fontSize: '2.5rem', mr:1}}/>
+                                <LinkedInIcon sx={{fontSize: '1.5rem', mr:1}}/>
                             </IconButton>
                             <IconButton sx={{color:'#fff'}} href="http://github.com/osSidd" target='_blank'>
-                                <GitHubIcon sx={{fontSize: '2.5rem'}}/>
+                                <GitHubIcon sx={{fontSize: '1.5rem'}}/>
                             </IconButton>
                             <IconButton sx={{color:'#fff'}} href="http://twitter.com" target='_blank'>
-                                <TwitterIcon sx={{fontSize: '2.5rem'}}/>
+                                <TwitterIcon sx={{fontSize: '1.5rem'}}/>
                             </IconButton>
                             <IconButton sx={{color:'#fff'}} href="https://www.instagram.com/" target='_blank'>
-                                <InstagramIcon sx={{fontSize: '2.5rem'}}/>
+                                <InstagramIcon sx={{fontSize: '1.5rem'}}/>
                             </IconButton>
                         </Box>
                     </Grid>
@@ -101,7 +103,7 @@ export default function Footer(){
             <Typography
                 sx={{
                     color:'#eee',
-                    fontSize: '14px',
+                    fontSize: '12px',
                     textAlign: 'center'
                 }}
             >
