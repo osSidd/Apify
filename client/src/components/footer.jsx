7fc2ab9706b1073024819fed165f0ac4d77c738e
Osama <107386980+osSidd@users.noreply.github.com>
2024-04-29 13:36:13 +0530

    footer responsive

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,38 +1,46 @@
-import { Container, Box, Typography, Link, Grid, Button, IconButton, TextField, TextareaAutosize } from '@mui/material'
-import { Form, Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+
+import { Container, Box, Typography, Link, Button, IconButton } from '@mui/material'
+
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import portfolio from '../assets/portfolio.svg';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import LogoText from './navbar/logotext'
-import NavLinks from './navbar/links'
 
 export default function Footer(){
 
-    // const links = ['about','credits','tools', 'download','blog']
     const links = ['about','news', 'weather']
 
     return(
         <Container 
             maxWidth='xl'
             sx={{
-                bgcolor: '#333',     
-                py:1,
+                bgcolor: '#282828',     
             }} 
         >
             <Box
                 sx={{
-                    mb:2,
                     py:3,
                     borderBottom:'1px solid #555'
                 }}
                 display='flex'
                 justifyContent='space-between'
                 alignItems='center'
+                flexDirection={{
+                    xs:'column',
+                    sm:'row'
+                }}
             >
-                <Box width='45%'>
-                    <LogoText logoSize={24} textSize={22}/>
+                <Box width={{xs:'100%', sm:'45%'}} textAlign={{xs:"center", sm:'left'}}>
+
+                    {/* Logo and text */}
+
+                    <Box mx={{xs:'auto', sm:'0'}} width='fit-content'>
+                        <LogoText logoSize={24} textSize={22}/>
+                    </Box>
+
+                    {/* Description */}
+
                     <Typography
                         sx={{color: '#eee'}}
                         mt={1}
@@ -41,89 +49,56 @@ export default function Footer(){
                         An api integration project which demonstrates integration of public apis and brings news feeds, weather updates under a single hood. 
                     </Typography>
                 </Box>
+                <Box mt={{xs:4, sm:0}}>
 
-                {/* <Grid container alignItems="start" px={5} columnGap={15}>
-                    <Grid item xs={12} md={12} mb={2} display="flex" flexDirection="row" alignItems="center" justifyContent="center">  */}
-                    {/* </Grid>
-                    <Grid item xs={12} md={12}> */}
-                        {/* <Typography
-                            sx={{color: '#eee'}}
-                            variant='h6'
-                            mb={2}
-                            alignSelf="start"
-                        >
-                            Pages
-                        </Typography> */}
-                        {/* <Box display="flex" flexDirection="row" alignItems="center">
-                            <NavLinks/>
-                        </Box> */}
-                    {/* </Grid>
-                    <Grid item xs={12} md={12} display="flex" flexDirection="row" justifyContent="center"> */}
-                        {/* <Typography
-                            sx={{color: '#eee'}}
-                            variant='h6'
-                            mb={2}
-                        >
-                            Links
-                        </Typography> */}
-                        <Box>
-                            <Box>
-                                {
-                                    links.map(link => (
-                                        <Button
-                                            component={RouterLink}
-                                            sx={{color:'#ddd', fontSize:'12px'}}
-                                            key={link}                                        
-                                        >
-                                            {link}
-                                        </Button>
-                                    ))
-                                }
-                            </Box>
-                            <Box textAlign='center'>
-                                <IconButton title='Linkedin' sx={{color:'#fff'}} href='https://www.linkedin.com/in/osama-siddiquee-5722a1a1/' target='_blank'>
-                                    <LinkedInIcon sx={{fontSize: '1.5rem', mr:1}}/>
-                                </IconButton>
-                                <IconButton title='github' sx={{color:'#fff'}} href="http://github.com/osSidd" target='_blank'>
-                                    <GitHubIcon sx={{fontSize: '1.5rem', mr:1}}/>
-                                </IconButton>
-                                <IconButton title='portfolio' sx={{color:'#fff'}} href="http://os-sidd.vercel.app" target='_blank'>
-                                    <Box
-                                        component='img'
-                                        src={portfolio}
-                                        width={24}
-                                        height={24}
-                                    />
-                                </IconButton>
-                                {/* <IconButton sx={{color:'#fff'}} href="http://twitter.com" target='_blank'>
-                                    <TwitterIcon sx={{fontSize: '1.5rem'}}/>
-                                </IconButton>
-                                <IconButton sx={{color:'#fff'}} href="https://www.instagram.com/" target='_blank'>
-                                    <InstagramIcon sx={{fontSize: '1.5rem'}}/>
-                                </IconButton> */}
+                    {/* Navigation and page links */}
+
+                    <Box>
+                        {
+                            links.map(link => (
+                                <Button
+                                    component={RouterLink}
+                                    sx={{color:'#ddd', fontSize:{xs:10, sm:12}}}
+                                    key={link}                                        
+                                >
+                                    {link}
+                                </Button>
+                            ))
+                        }
+                    </Box>
+
+                    {/* Social icons */}
+
+                    <Box textAlign='center'>
+                        <IconButton title='Linkedin' sx={{color:'#fff'}} href='https://www.linkedin.com/in/osama-siddiquee-5722a1a1/' target='_blank'>
+                            <LinkedInIcon sx={{fontSize: '1.5rem', mr:1}}/>
+                        </IconButton>
+                        <IconButton title='github' sx={{color:'#fff'}} href="http://github.com/osSidd" target='_blank'>
+                            <GitHubIcon sx={{fontSize: '1.5rem', mr:1}}/>
+                        </IconButton>
+                        <IconButton title='portfolio' sx={{color:'#fff'}} href="http://os-sidd.vercel.app" target='_blank'>
+                            <Box
+                                component='img'
+                                src={portfolio}
+                                width={24}
+                                height={24}
+                            />
+                        </IconButton>
                     </Box>
                 </Box>
-                    {/* </Grid>
-                    <Grid item xs={12} md={12} display="flex" flexDirection="column"> */}
-                        {/* <Typography
-                            sx={{color: '#eee'}}
-                            variant='h6'
-                            mb={2}
-                        >
-                            Get in Touch
-                        </Typography> */}
-                        {/* <TextField size='small' sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Name'/>
-                        <TextField size='small' sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Email'/>
-                        <TextField size='small' multiline rows={4} sx={{color: '#eee', bgcolor: '#cdcdcd', borderRadius: '5px', marginBottom: '12px'}} placeholder='Message'/>
-                        <Button variant='contained' color='primary'>Send Message</Button> */}
-                    {/* </Grid>
-                </Grid> */}
             </Box>
+
+            {/* copyright section */}
+
             <Box>
                 <Typography
                     sx={{
                         color:'#eee',
-                        fontSize: '12px',
+                    }}
+                    py={2}
+                    fontSize={{
+                        xs:10,
+                        sm:12
                     }}
                     textAlign='center'
                 >
