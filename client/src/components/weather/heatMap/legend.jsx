291397aa1753c4613677d1f80829b3c6b35f66d8
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-05 22:45:58 +0530

    added precipitation description in legend

--- client/src/components/weather/heatMap/legend.jsx
+++ client/src/components/weather/heatMap/legend.jsx
@@ -11,6 +11,40 @@ export default function Legend({minutely}){
         makeChart()
     }, [minutely])
 
+    function getPopTime(data){
+        const now = data[0]
+        const pop = data.filter(d => d.precipitation)
+        const end = data.find((d,i) => d.precipitation && i)
+        const fullHr = data.find((d,i) => !d.precipitation && i)
+        
+        if(!pop.length) return 'No precipitation within an hour'
+
+        let mins = new Date(end.dt*1000) - new Date(now.dt*1000)
+        let int = parseInt(mins/(60*1000))
+
+        if(data[0].precipitation){
+            if(!fullHr) return "Precipitation won't end within an hour"
+
+            return `Precipitation will end in ${int > 1 ? int + ' mins' : int + ' min'}`
+        }
+
+        else{
+            return `Precipitation will start within ${int > 1 ? int + ' mins' : int + ' min'}`
+        }
+    }
+
+    function getColor(d){
+        let prep = d.precipitation
+
+        if(prep >= 140) return 'rgba(20, 20, 255, 0.9)'
+        if(prep >= 10) return 'rgba(80,80, 225, 0.7)'
+        if(prep >= 1) return '#00d300'
+        if(Math.abs(prep - 0.5) < 0.01) return '#00fa64'
+        if(Math.abs(prep - 0.2) < 0.01) return '#64fcbf'
+        if(Math.abs(prep - 0.1) < 0.01) return '#b5fee1'
+        return '#9b9b9b'
+    }
+
     function makeChart(){
         const height = 50
         const width = 425
@@ -23,6 +57,8 @@ export default function Legend({minutely}){
                         .attr('width', width)
         
         svg.selectAll('*').remove()
+
+        const maxRain = d3.max(minutely, d => d.precipitation)
 
         const xScaleMajor = d3.scaleBand()
                                 .domain(minutely.map(d => d))
@@ -48,6 +84,7 @@ export default function Legend({minutely}){
                             return 5
                         return 0
                     })
+                    .style('stroke', d => getColor(d))
 
        const  timeContainer = svg.append('g')
         timeContainer.selectAll('text')
@@ -72,7 +109,7 @@ export default function Legend({minutely}){
 
     return (
         <Box position='absolute' bottom={10} left={10} bgcolor='white' width='75%' boxShadow={5} borderRadius={1} zIndex={10} sx={{cursor:'pointer'}}>
-            <Typography ml={1} mt={1} fontSize={14} fontWeight={500}>No precipitation within an hour</Typography>
+            <Typography ml={1} mt={1} fontSize={14} fontWeight={500}>{getPopTime(minutely)}</Typography>
             <svg ref={legendRef}></svg>
         </Box>
     )
