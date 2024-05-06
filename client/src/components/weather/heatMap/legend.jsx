6d95f764703c347b94879f131e6edfacf1886b01
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-06 12:55:11 +0530

    map legend logic complete

--- client/src/components/weather/heatMap/legend.jsx
+++ client/src/components/weather/heatMap/legend.jsx
@@ -7,6 +7,35 @@ export default function Legend({minutely}){
     
     const legendRef = useRef()
 
+    function getLegend(){
+
+        let legendColors = []
+        
+        const minRain = d3.min(minutely, d => d.precipitation)
+        const maxRain =  d3.max(minutely, d => d.precipitation)
+        const div = maxRain/4
+
+        if (!maxRain) return [getColor(maxRain)]
+
+        if(maxRain >= 2){
+            for(let i = minRain; i <= maxRain; i+= div){
+                const col = getColor(i)
+
+                if(!legendColors.find(d => d.col === col.col)) legendColors.push(getColor(i))
+            }
+        }
+
+        else{
+            for(let i = 0; i <= maxRain; i+= div){
+                const col = getColor(i)
+                
+                if(!legendColors.find(d => d.col === col.col)) legendColors.push(getColor(i))
+            }
+        }
+
+        return legendColors
+    }
+
     useEffect(() => {
         makeChart()
     }, [minutely])
@@ -14,18 +43,18 @@ export default function Legend({minutely}){
     function getPopTime(data){
         const now = data[0]
         const pop = data.filter(d => d.precipitation)
-        const end = data.find((d,i) => d.precipitation && i)
+        // const end = data.find((d,i) => d.precipitation && i)
         const fullHr = data.find((d,i) => !d.precipitation && i)
         
         if(!pop.length) return 'No precipitation within an hour'
 
-        let mins = new Date(end.dt*1000) - new Date(now.dt*1000)
+        let mins = new Date(fullHr.dt*1000) - new Date(now.dt*1000)
         let int = parseInt(mins/(60*1000))
 
         if(data[0].precipitation){
             if(!fullHr) return "Precipitation won't end within an hour"
 
-            return `Precipitation will end in ${int > 1 ? int + ' mins' : int + ' min'}`
+            return `Precipitation will end within ${int > 1 ? int + ' mins' : int + ' min'}`
         }
 
         else{
@@ -33,16 +62,15 @@ export default function Legend({minutely}){
         }
     }
 
-    function getColor(d){
-        let prep = d.precipitation
+    function getColor(prep){
 
-        if(prep >= 140) return 'rgba(20, 20, 255, 0.9)'
-        if(prep >= 10) return 'rgba(80,80, 225, 0.7)'
-        if(prep >= 1) return '#00d300'
-        if(Math.abs(prep - 0.5) < 0.01) return '#00fa64'
-        if(Math.abs(prep - 0.2) < 0.01) return '#64fcbf'
-        if(Math.abs(prep - 0.1) < 0.01) return '#b5fee1'
-        return '#9b9b9b'
+        if(prep >= 140) return {mm:140, col:'#00ff00'}
+        if(prep >= 10) return {mm:10, col:'#00ff00dd'}
+        if(prep >= 1) return {mm:1, col:'#00ff00aa'}
+        if(prep >= 0.5) return {mm:0.5, col:'#00ff0099'}
+        if(prep >= 0.2) return {mm:0.2, col:'#00ff0077'}
+        if(prep >= 0.1) return {mm:0.1, col:'#00ff0055'}
+        return {mm:0, col:'#9b9b9b'}
     }
 
     function makeChart(){
@@ -55,10 +83,9 @@ export default function Legend({minutely}){
         const svg = d3.select(legendRef.current)
                         .attr('height', height)
                         .attr('width', width)
+                        // .style('background-color', 'pink')
         
         svg.selectAll('*').remove()
-
-        const maxRain = d3.max(minutely, d => d.precipitation)
 
         const xScaleMajor = d3.scaleBand()
                                 .domain(minutely.map(d => d))
@@ -84,7 +111,7 @@ export default function Legend({minutely}){
                             return 5
                         return 0
                     })
-                    .style('stroke', d => getColor(d))
+                    .style('stroke', d => getColor(d.precipitation)['col'])
 
        const  timeContainer = svg.append('g')
         timeContainer.selectAll('text')
@@ -107,10 +134,25 @@ export default function Legend({minutely}){
                     .style('font-size', 10)
     }
 
+    const legendArr = getLegend() 
+    console.log(legendArr)
+
     return (
         <Box position='absolute' bottom={10} left={10} bgcolor='white' width='75%' boxShadow={5} borderRadius={1} zIndex={10} sx={{cursor:'pointer'}}>
             <Typography ml={1} mt={1} fontSize={14} fontWeight={500}>{getPopTime(minutely)}</Typography>
-            <svg ref={legendRef}></svg>
+            <Box display='flex' justifyContent='space-between' alignItems='flex-end'>
+                <svg ref={legendRef}></svg>
+                <Box pr={1} pb={1}>
+                    {
+                        legendArr.map(d => (
+                            <Box key={d.mm} display='flex' justifyContent='space-between' alignItems='center'>
+                                <Box mr={1} width={6} height={6} bgcolor={d.col}></Box>
+                                <Typography fontSize={8}>{d.mm} mm/h</Typography>
+                            </Box>
+                        ))
+                    }
+                </Box>
+            </Box>
         </Box>
     )
 }
