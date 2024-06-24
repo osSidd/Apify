e0f4dee38b0e6407bb5ec9b2e60ea9d5e872fe68
Osama <107386980+osSidd@users.noreply.github.com>
2024-06-24 22:32:24 +0530

    air quality index
    initial base component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 import Footer from "./components/footer"
 import WeatherPage from "./pages/weather/weatherpage"
+import Aqi from "./components/weather/aqi/aqi"
 
 class App extends Component{
   render(){
@@ -12,6 +13,7 @@ class App extends Component{
       <ThemeProvider theme={theme}>
         <WeatherPage/>
         <Footer/> 
+        <Aqi/>
       </ThemeProvider>  
     )
   }
---
+++ client/src/components/weather/aqi/aqi.jsx
@@ -0,0 +1,9 @@
+import {Box, Typography} from '@mui/material'
+
+export default function Aqi(){
+    return (
+        <Box>
+            <Typography>Air Quality Index</Typography>
+        </Box>
+    )
+}
